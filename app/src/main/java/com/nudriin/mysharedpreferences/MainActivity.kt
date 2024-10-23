1a8d29b48816174b2b26118bfae84c7cf233ec7b
Nurdin <mrsunnysummer@gmail.com>
2024-10-23 14:08:47 +0700

    style: create app layout

---
+++ app/src/main/java/com/nudriin/mysharedpreferences/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.nudriin.mysharedpreferences
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+//    private lateinit var binding: Actii
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
